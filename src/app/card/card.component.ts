dacbf3805c951d42553e94933164c08d873c7d38
Emanuele Biselli <emanuele.biselli@pegaso2000.it>
2023-06-30 13:25:10 +0200

    card aggiornata intermittente e bottone intermittente

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 
@@ -12,7 +12,24 @@ import {MatCardModule} from '@angular/material/card';
   standalone: true,
   imports: [MatCardModule, MatButtonModule],
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
+  immagine = ''
+  immagine1 = 'https://www.bing.com/th?id=OAIP.44141d8dbac9a1485e7923fb8977577b&pid=AdsNative&c=3&w=300&h=157&dynsize=1'
+  immagine2 = 'https://material.angular.io/assets/img/examples/shiba2.jpg'
+
+  ngOnInit(): void {
+    console.log("ngoninit")
+    let counter = 0
+    setInterval(()=>{
+      if(counter%2 == 0){
+        this.immagine = this.immagine1
+      }else{
+        this.immagine = this.immagine2
+      }
+      counter++
+    }, 1000)
+  }
+  
 cani = [
   {nome : 'rogero',
   razza : 'golden',
