f2581269e14c44b1204070777a65baeb577da5f1
Emanuele Biselli <emanuele.biselli@pegaso2000.it>
2023-07-03 10:00:30 +0200

    directive ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'testb';
   isVisible = true;
+  num = 3;
+  string = "topolino";
 
   persone = [
     {nome: "luca", cognome: "rossi", isOnline: true},
